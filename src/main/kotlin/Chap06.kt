c1c4c4d6a095781e7fbd1aa614ab88f024f936df
itc-s23011 <s23011@std.it-college.ac.jp>
2024-05-15 12:26:54 +0900

    7章まで終わり

--- src/main/kotlin/Chap06.kt
+++ src/main/kotlin/Chap06.kt
@@ -7,5 +7,12 @@ fun main(array: Array<String>) {
     println("変数 a の値は $a です。")
     println("変数 a の値は $b です。")
 
+    println("----6.6----")
+    val num61 = 5
+    val num62 = 10
+    val a6 = (num61 < num62)
+    val b6 = (num61 > num62)
+    println("num61が num62より小さい:$a6")
+    println("num61が num62より大きい:$b6")
 
 }
